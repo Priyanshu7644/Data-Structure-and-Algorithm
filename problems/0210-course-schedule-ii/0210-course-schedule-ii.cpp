c65b784181c11b6763730e849f5d11a3bf6eb7a7
Priyanshu Kumar <152106628+Priyanshu7644@users.noreply.github.com>
2026-07-21 09:53:58 +0000

    LeetCode Synchronization - Course Schedule II (C++)

---
+++ problems/0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
+        for(auto edge:prerequisites){
+            int course=edge[0];
+            int pre=edge[1];
+            adj[pre].push_back(course);
+            indegree[course]++;
+        }
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0)
+                q.push(i);
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto nbr:adj[node]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0)
+                    q.push(nbr);
+            }
+        }
+        if(ans.size()==numCourses)
+            return ans;
+        return {};
+    }
+};
